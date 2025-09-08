51be5f259365e75410fbd74270bedc5ec3953525
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-09-08 19:01:51 +0200

    feature(home): add our clients & tailwind styles

--- src/components/OurClients.jsx
+++ src/components/OurClients.jsx
@@ -1,5 +1,19 @@
 export const OurClients = () => {
     return (
-        <h2>Hola</h2>
+        <div className="text-center py10 p-5">
+            <h3 className="text-3xl font-semibold mb-2">Our Clients</h3>
+            <p className="text-gray-600 mb-8 text-xl">We have been working with some Fortune 500+ clients</p>
+
+            <div className="flex flex-wrap justify-center items-center gap-6">
+                <img src="/src/media/logo/logo-one.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
+                <img src="/src/media/logo/logo-two.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
+                <img src="/src/media/logo/logo-three.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
+                <img src="/src/media/logo/logo-four.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
+                <img src="/src/media/logo/logo-five.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
+                <img src="/src/media/logo/logo-six.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
+                <img src="/src/media/logo/logo-seven.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
+            </div>
+        
+        </div>
     )
 }
