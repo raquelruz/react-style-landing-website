d0e1e6c252f7446863947278194915af430da1a5
Raquel Ruiz <raquelruizlopezcepero@gmail.com>
2025-09-08 19:02:41 +0200

    feature(home): change size for text

--- src/components/OurClients.jsx
+++ src/components/OurClients.jsx
@@ -2,7 +2,7 @@ export const OurClients = () => {
     return (
         <div className="text-center py10 p-5">
             <h3 className="text-3xl font-semibold mb-2">Our Clients</h3>
-            <p className="text-gray-600 mb-8 text-xl">We have been working with some Fortune 500+ clients</p>
+            <p className="text-gray-600 mb-8 text-sm">We have been working with some Fortune 500+ clients</p>
 
             <div className="flex flex-wrap justify-center items-center gap-6">
                 <img src="/src/media/logo/logo-one.png" alt="" className="h-12 transition-transform duration-300 hover:scale-110"/>
